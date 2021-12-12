451e4fe61e016c352067e6cfddb40504f6623cb8
Chinmay Shringi <chinmay.logicwind@gmail.com>
2021-12-12 22:39:11 +0530

    context

--- router/src/components/Fetch.js
+++ router/src/components/Fetch.js
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Fetch() {
+  useEffect(() => {
+    callRequest();
+    item = "123";
+  }, []);
+
+  var item = "14";
   const callRequest = async () => {
-    console.log(fetch("https://jsonplaceholder.typicode.com/todos/1"));
+    // console.log(fetch("https://jsonplaceholder.typicode.com/todos/1"));
     // PROMISE ^
 
-    let data;
-    data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-    console.log(data);
+    // let data;
+    // data = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    // console.log(data);
     // RESPONSE ^
 
-    let item = await data.json();
-    console.log(item);
+    // item = await data.json();
+    // console.log(item);
     // OBJECT ^
+
+    fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response_value) => {
+        response_value.json().then((object_response) => {
+          item = object_response;
+          console.log(object_response);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  return <button onClick={callRequest}>Call Request</button>;
+  return (
+    <div>
+      <p>{item}</p>
+      <button onClick={callRequest}>Call1 Request</button>
+    </div>
+  );
 }
 
 export default Fetch;
